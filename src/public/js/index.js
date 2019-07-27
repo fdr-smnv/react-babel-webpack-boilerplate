ad2a51fa96758b2eb530fb707de941e8377cce08
fdr-smnv <fdr.smnv@outlook.com>
2019-07-27 19:04:18 +0300

    Renamed main app component file to index.js

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
+
 const App = () => <h1>Hello, React!</h1>;
 
 ReactDom.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, "src", "public", "js", "main.js"),
+    entry: path.resolve(__dirname, "src", "public", "js", "index.js"),
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
