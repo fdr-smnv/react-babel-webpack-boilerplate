f10d249e14705ef5b37852f22153582e30fe7fb6
fdr-smnv <fdr.smnv@outlook.com>
2019-07-27 17:37:30 +0300

    Dev mode set and output added

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,9 @@ const path = require('path');
 
 module.exports = {
     entry: path.resolve(__dirname, "src", "public", "js", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    mode: "development"
 }
