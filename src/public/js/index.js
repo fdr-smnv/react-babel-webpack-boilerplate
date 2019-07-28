84549c647741d3bf0641a4b2c6897f1d1c46d55f
fdr-smnv <fdr.smnv@outlook.com>
2019-07-28 15:03:02 +0300

    Added react logo to App

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import '../css/index.css'
+import logo from '../img/react-icon.svg'
 
 const App = () => (
     <div className="outer">
         <div className="inner">
             <h1>Hello, React!</h1>
+            <img src={logo}></img>
         </div>
     </div>
 );
